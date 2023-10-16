323585443439d1a8a88ac5c7aa58212bfbb56b23
trinidadmcdavid <rupaislam0155@gmail.com>
2023-10-16 22:16:22 +0600

    update

--- wp-content/themes/wonderful-blog/header.php
+++ wp-content/themes/wonderful-blog/header.php
@@ -13,6 +13,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	<meta name="google-site-verification" content="4rEgtQx6sIadGqz1ONf-QCUgRXtQoSSFfiQptMaW7FM" />
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
